d11bef928769a8fa90f9c535077a22a4d7ec5535
Zeeshan-CS <zeshanishfaq1@gmail.com>
2021-04-19 14:15:09 +0500

    arrays and ASCII and filehandling(inprogress)

--- ASCII.cpp
+++ ASCII.cpp
@@ -6,9 +6,9 @@ int main()
 {
     string data = "Hello World";
     int i;
-
+    cout<<"ASCII value of Hello World " ;
     for (i = 0; i < 11; i++)
     {
-        cout << data[i] << "   ASCII value   " << int(data[i]) << endl;
+        cout<< int(data[i])  ;
     }
 }
---
+++ filecases.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    fstream my_file;
+    string text;
+    my_file.open("zia_bhai.txt", ios::out);
+    if (!my_file)
+    {
+        cout << "File not created!";
+    }
+    else
+    {
+        cout << "File created successfully!" << endl;
+        text = "LorEm IpsUm is simply dUmMY TeXt of thE prIntiNg aNd tYpeseTTing inDUStry. LOREm IpsuM has been the industry's standard dummy text ever since the 1500s,  when an unknown printer took a galley of type and scrambled it to make a type specimen book ";
+        my_file << text;
+        my_file.close();
+    }
+
+    my_file.open("zia_bhai.txt", ios::in);
+    if (!my_file)
+    {
+        cout << "No such file";
+    }
+    else
+    {
+        int i;
+        string text;
+
+        int count = 0;
+        while (getline(my_file, text, '\n'))
+        {
+            cout<<text;
+        }
+        my_file.close();
+    }
+}
