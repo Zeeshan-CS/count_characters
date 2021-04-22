bb8079d7614fb754c0260e76b52b5df1de7da649
Zeeshan-CS <zeshanishfaq1@gmail.com>
2021-04-23 03:19:27 +0500

    fun_overloading_completed

--- sorting_fun( )_overload.cpp
+++ sorting_fun( )_overload.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
+/*Write a C++ program (using function overloaded) to sort 10 integer values, 
+or 10 long values, or 10 double values.*/
 
 void sort(int x[10])
 {
-    //for (int i = 0; i < 5; ++i) {
-    //  cout << "Student " << i + 1 << ": " << m[i] << endl;
-
     int i, j, temp;
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 10 - 2; i++)
     {
-        for (j = 0; j < 10 - i; j++)
+        for (j = 0; j < 10 - i - 1; j++)
         {
             if (x[j] > x[j + 1])
             {
                 temp = x[j];
                 x[j] = x[j + 1];
                 x[j + 1] = temp;
-                //swapping using 2 var
-                // x[j] = x[j] + x[j+1];
-                // x[j+1] = x[j] - x[j+1];
-                // x[j] = x[j] - x[j+1];
             }
         }
     }
@@ -31,26 +26,89 @@ void sort(int x[10])
         cout << x[i] << " ";
     }
 }
-// void sort(float x[])
-// {
-// }
-// void sort(double x[])
-// {
-// }
+void sort(float x[10])
+{
+    int i, j, temp;
+
+    for (i = 0; i < 10 - 2; i++)
+    {
+        for (j = 0; j < 10 - i - 1; j++)
+        {
+            if (x[j] > x[j + 1])
+            {
+                temp = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp;
+            }
+        }
+    }
+    cout << " Sorted array ";
+    for (i = 0; i < 10; i++)
+    {
+        cout << x[i] << " ";
+    }
+}
+void sort(double x[10])
+{
+    int i, j, temp;
+
+    for (i = 0; i < 10 - 2; i++)
+    {
+        for (j = 0; j < 10 - i - 1; j++)
+        {
+            if (x[j] > x[j + 1])
+            {
+                temp = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp;
+            }
+        }
+    }
+    cout << " Sorted array ";
+    for (i = 0; i < 10; i++)
+    {
+        cout << x[i] << " ";
+    }
+}
 
 int main()
 {
-    int list1[10];
+    double list1[10];
     float list2[10];
     double list3[10];
     int x;
-    int y;
+    int choice;
 
-    for (x = 0; x < 10; x++)
+    cout << " 1 for integer array ";
+    cout << " 2 for float array ";
+    cout << " 3 for double array ";
+    cin >> choice;
+
+    if (choice == 1)
     {
-        cout << " Enter the NUmbers for sorting ";
-        cin >> list1[x];
+        for (x = 0; x < 10; x++)
+        {
+            cout << " Enter the NUmbers for sorting ";
+            cin >> list1[x];
+        }
+        sort(list1);
     }
-
-    sort(list1);
+    else if (choice == 2)
+    {
+        for (x = 0; x < 10; x++)
+        {
+            cout << " Enter the NUmbers for sorting ";
+            cin >> list2[x];
+        }
+        sort(list2);
+    }
+    else if (choice == 3)
+    {
+        for (x = 0; x < 10; x++)
+        {
+            cout << " Enter the NUmbers for sorting ";
+            cin >> list3[x];
+        }
+        sort(list2);
+    }
 }
